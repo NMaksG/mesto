a05dcecaf103f14bd058e0897304d8dcde461234
Maksim Nayda <nmaximus@yandex.ru>
2022-04-14 07:16:00 +0300

    JSadd NewCard/delCard

--- scripts/index.js
+++ scripts/index.js
@@ -28,15 +28,18 @@ const popupModalWindow = document.querySelector('.popup');
 const popupModalWindowEdit = document.querySelector('.popup_edit');
 const buttonOpenPopupEdit = document.querySelector('.profile__edit-button');
 const buttonClosePopupEdit = popupModalWindowEdit.querySelector('.popup__close');
-const popupForm = document.querySelector('.popup__content');
-const popupName = popupForm.querySelector('.popup__field_type_name');
-const popupDescription = popupForm.querySelector('.popup__field_type_description');
+const popupFormEdit = popupModalWindowEdit.querySelector('.popup__content');
+const popupName = popupFormEdit.querySelector('.popup__field_type_name');
+const popupDescription = popupFormEdit.querySelector('.popup__field_type_description');
 const profName = document.querySelector('.profile__name');
 const profDescription = document.querySelector('.profile__description');
 const buttonOpenPopupAdd = document.querySelector('.profile__add-button');
 
 const popupModalWindowAdd = document.querySelector('.popup_add');
 const buttonClosePopupAdd = popupModalWindowAdd.querySelector('.popup__close');
+const popupFormAdd = popupModalWindowAdd.querySelector('.popup__content');
+const popupTitle = popupFormAdd.querySelector('.popup__field_type_title');
+const popupLink = popupFormAdd.querySelector('.popup__field_type_link');
 
 
 function addModalWindow(popupModalWindow) {
@@ -57,7 +60,7 @@ function closeModalWindow(popupModalWindow) {
 buttonClosePopupEdit.addEventListener('click', function () {closeModalWindow(popupModalWindowEdit)});
 buttonClosePopupAdd.addEventListener('click',  function () {closeModalWindow(popupModalWindowAdd)});
 
-function oneSubmit(e) {
+function handleEditSubmit(e) {
   e.preventDefault();
   
   profName.textContent = popupName.value;
@@ -65,7 +68,7 @@ function oneSubmit(e) {
   
   closeModalWindow(popupModalWindow);
 }
-popupForm.addEventListener('submit', oneSubmit);
+popupFormEdit.addEventListener('submit', handleEditSubmit);
 
 const listCards = document.querySelector('.elements__element_template');
 const template = document.querySelector('.template');
@@ -74,13 +77,36 @@ function render() {
   const html = initialCards.map(getElement);
   listCards.append(...html);
 }
+render();
 
 function getElement(item) {
   const getElementTemplate = template.content.cloneNode(true);
   const img = getElementTemplate.querySelector('.elements-item__img_template');
-  img.src = item.link;
   const title = getElementTemplate.querySelector('.elements-item__title_template');
+  const buttonDelCard = getElementTemplate.querySelector('.elements-item__del_card');
+
+  img.src = item.link;
+  img.alt = item.name;
   title.textContent = item.name;
+
+  buttonDelCard.addEventListener('click', handleDelCard);  
+
   return getElementTemplate;
 }
-render();
+
+function handleDelCard(e) {
+  const delCard = e.target.closest('.elements-item');
+  delCard.remove();
+}
+
+function handleAddSubmit(e) {
+  e.preventDefault();
+ 
+  const TitleNewCard = popupTitle.value;
+  const LinkNewCard = popupLink.value;
+  const NewCard = getElement({name: TitleNewCard, link: LinkNewCard});
+  listCards.prepend(NewCard);
+  
+  closeModalWindow(popupModalWindowAdd);
+}
+popupFormAdd.addEventListener('submit', handleAddSubmit);
